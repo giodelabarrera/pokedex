1c16e44791329e2ca88bf87f5137c54fcdb35775
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-07-17 17:26:10 +0200

    Configure react quick version

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <p>lorem ipsum</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
