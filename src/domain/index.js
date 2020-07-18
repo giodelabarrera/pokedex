404b98916294993a64bbb78be8f6760a9627ee44
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-07-18 11:59:51 +0200

    Create domain with pokemon context

---
+++ src/domain/index.js
@@ -0,0 +1,31 @@
+const USE_CASES = {
+  pokemon__get_list_use_case: () =>
+    import(
+      /* webpackChunkName: "pokemon__get_list_use_case" */
+      './tierlist/useCases/GetTierlistUseCase/factory'
+    ),
+  pokemon__get_single_use_case: () =>
+    import(
+      /* webpackChunkName: "pokemon__get_single_use_case" */
+      './tierlist/useCases/GetFilteredTierlistUseCase/factory'
+    ),
+  pokemon__get_types_use_case: () =>
+    import(
+      /* webpackChunkName: "pokemon__get_types_use_case" */
+      './tierlist/useCases/GetNextGameRouteUseCase/factory'
+    ),
+}
+
+const entryPoint = {
+  get: useCaseName => {
+    return {
+      async execute(...params) {
+        const {default: useCaseFactory} = await USE_CASES[useCaseName]()
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        return useCaseFactory().execute(...params)
+      },
+    }
+  },
+}
+
+export default entryPoint
