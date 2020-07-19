c926b0816ad5505dabcfeb3577ada6ed08863ddf
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-07-19 23:42:31 +0200

    Add implementation of GetListUseCase

--- src/domain/pokemon/repositories/RESTRepository/index.js
+++ src/domain/pokemon/repositories/RESTRepository/index.js
@@ -4,20 +4,20 @@ export default function RESTRepository ({ httpClient }) {
       const pokemon = await httpClient(`pokemon/${idOrName}`)
       return pokemon
     },
-    async getList ({ query, types = [], sort = 'lowest_number', limit = 12, offset = 0 }) {
-      function toQueryString (params) {
+    async getList ({ query = '', types = [], sort = 'lowest_number', limit = 12, offset = 0 } = {}) {
+      function toQueryString ({ query, types, sort, limit, offset }) {
         const searchParams = new URLSearchParams()
-        if (query) searchParams.append('query', params.query)
-        searchParams.append('types', params.types)
-        searchParams.append('sort', params.sort)
-        searchParams.append('limit', params.limit)
-        searchParams.append('offset', params.offset)
+        if (query) searchParams.append('query', query)
+        if (types.length) types.forEach(type => searchParams.append('types[]', type))
+        searchParams.append('sort', sort)
+        searchParams.append('limit', limit)
+        searchParams.append('offset', offset)
         return searchParams.toString()
       }
 
       const params = { query, types, sort, limit, offset }
       const queryString = toQueryString(params)
-      const endpoint = 'pokemon' + queryString && `?${queryString}`
+      const endpoint = 'pokemon' + (queryString && `?${queryString}`)
       const pokemonList = await httpClient(endpoint)
       return pokemonList
     }
