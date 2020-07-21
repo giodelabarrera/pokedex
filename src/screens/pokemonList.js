11fb8b1e935b11c6244ad792a93adb1df5a03ccf
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-07-21 18:19:22 +0200

    Show pokemon with images

--- src/screens/pokemonList.js
+++ src/screens/pokemonList.js
@@ -2,6 +2,9 @@ import React, {useEffect, useState} from 'react'
 
 import {useDomain} from '../context/domainProvider'
 
+const IMAGE_BASE_URL =
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/detail'
+
 export default function PokemonListScreen() {
   const domain = useDomain()
 
@@ -15,9 +18,14 @@ export default function PokemonListScreen() {
   }, [domain])
 
   return (
-    <>
-      <h1>Pokemon list</h1>
-      <pre>{JSON.stringify(pokemonList, null, 2)}</pre>
-    </>
+    <div className="pk-PokemonList">
+      {pokemonList.map(({id, number, name, types, stats}) => (
+        <div key={id}>
+          <img src={`${IMAGE_BASE_URL}/${number}.png`} alt={name} />
+          <h4>{name}</h4>
+          <span>{number}</span>
+        </div>
+      ))}
+    </div>
   )
 }
