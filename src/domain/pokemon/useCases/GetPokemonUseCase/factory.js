86bb07f63ba4271c6ababf3dd3b6fc49cae24761
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-07-22 01:20:09 +0200

    Return slug property for GetPokemonUseCase

--- src/domain/pokemon/useCases/GetPokemonUseCase/factory.js
+++ src/domain/pokemon/useCases/GetPokemonUseCase/factory.js
@@ -1,10 +1,12 @@
 import RESTRepositoryFactory from '../../repositories/RESTRepository/factory'
 import GetPokemonImageUrlsServiceFactory from '../../services/GetPokemonImageUrlsService/factory'
+import GetPokemonSlugServiceFactory from '../../services/GetPokemonSlugService/factory'
 import GetPokemonUseCase from '.'
 
 export default () => {
   return GetPokemonUseCase({
     pokemonRepository: RESTRepositoryFactory(),
-    getPokemonImageUrlsService: GetPokemonImageUrlsServiceFactory()
+    getPokemonImageUrlsService: GetPokemonImageUrlsServiceFactory(),
+    getPokemonSlugService: GetPokemonSlugServiceFactory()
   })
 }
--- src/domain/pokemon/useCases/GetPokemonUseCase/index.js
+++ src/domain/pokemon/useCases/GetPokemonUseCase/index.js
@@ -1,15 +1,20 @@
 export default function GetPokemonUseCase({
   pokemonRepository,
-  getPokemonImageUrlsService
+  getPokemonImageUrlsService,
+  getPokemonSlugService
 }) {
   return {
     async execute({idOrName}) {
       const pokemonEntity = await pokemonRepository.getPokemon({idOrName})
+      const slug = getPokemonSlugService.execute({
+        pokemonName: pokemonEntity.name
+      })
       const imageUrls = getPokemonImageUrlsService.execute({
         pokemonNumber: pokemonEntity.number()
       })
       return {
         ...pokemonEntity.toJson(),
+        slug,
         imageUrl: imageUrls.large
       }
     }
