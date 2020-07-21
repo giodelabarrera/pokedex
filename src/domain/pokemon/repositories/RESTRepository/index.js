143364556357f3850bb40614a4662117646a71b5
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-07-22 00:48:04 +0200

    Separate functions of RESTRepository

--- src/domain/pokemon/repositories/RESTRepository/index.js
+++ src/domain/pokemon/repositories/RESTRepository/index.js
@@ -3,48 +3,54 @@ export default function RESTRepository({
   pokemonResponseJsonToPokemonEntityJsonMapper,
   pokemonEntityFactory
 }) {
-  return {
-    async getPokemon({idOrName}) {
-      const pokemonResponseJson = await httpClient(`pokemon/${idOrName}`)
-      const pokemonEntityJson = pokemonResponseJsonToPokemonEntityJsonMapper.map(
-        pokemonResponseJson
-      )
-      const pokemonEntity = pokemonEntityFactory(pokemonEntityJson)
-      return pokemonEntity
-    },
-    async getPokemonList({
-      query = '',
-      types = [],
-      sort = 'lowest_number',
-      limit = 12,
-      offset = 0
-    } = {}) {
-      function toQueryString({query, types, sort, limit, offset}) {
-        const searchParams = new URLSearchParams()
-        if (query) searchParams.append('query', query)
-        if (types.length)
-          types.forEach(type => searchParams.append('types[]', type))
-        searchParams.append('sort', sort)
-        searchParams.append('limit', limit)
-        searchParams.append('offset', offset)
-        return searchParams.toString()
-      }
+  async function getPokemon({idOrName}) {
+    const pokemonResponseJson = await httpClient(`pokemon/${idOrName}`)
+    const pokemonEntityJson = pokemonResponseJsonToPokemonEntityJsonMapper.map(
+      pokemonResponseJson
+    )
+    const pokemonEntity = pokemonEntityFactory(pokemonEntityJson)
+    return pokemonEntity
+  }
 
-      const params = {query, types, sort, limit, offset}
-      const queryString = toQueryString(params)
-      const endpoint = 'pokemon' + (queryString && `?${queryString}`)
-      const pokemonResponseJsonList = await httpClient(endpoint)
-
-      // map from REST response json list to pokemon domain entity json list
-      const pokemonEntityJsonList = pokemonResponseJsonList.map(
-        pokemonResponseJsonToPokemonEntityJsonMapper.map
-      )
-      const pokemonEntityList = pokemonEntityJsonList.map(pokemonEntityFactory)
-      return pokemonEntityList
-    },
-    async getTypeList() {
-      const typeList = await httpClient('type')
-      return typeList
+  async function getPokemonList({
+    query = '',
+    types = [],
+    sort = 'lowest_number',
+    limit = 12,
+    offset = 0
+  } = {}) {
+    function toQueryString({query, types, sort, limit, offset}) {
+      const searchParams = new URLSearchParams()
+      if (query) searchParams.append('query', query)
+      if (types.length)
+        types.forEach(type => searchParams.append('types[]', type))
+      searchParams.append('sort', sort)
+      searchParams.append('limit', limit)
+      searchParams.append('offset', offset)
+      return searchParams.toString()
     }
+
+    const params = {query, types, sort, limit, offset}
+    const queryString = toQueryString(params)
+    const endpoint = 'pokemon' + (queryString && `?${queryString}`)
+    const pokemonResponseJsonList = await httpClient(endpoint)
+
+    // map from REST response json list to pokemon domain entity json list
+    const pokemonEntityJsonList = pokemonResponseJsonList.map(
+      pokemonResponseJsonToPokemonEntityJsonMapper.map
+    )
+    const pokemonEntityList = pokemonEntityJsonList.map(pokemonEntityFactory)
+    return pokemonEntityList
+  }
+
+  async function getTypeList() {
+    const typeList = await httpClient('type')
+    return typeList
+  }
+
+  return {
+    getPokemon,
+    getPokemonList,
+    getTypeList
   }
 }
