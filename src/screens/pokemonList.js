fe38bc382f28fd429f60efcc1e3c1fbf72bc1c46
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-07-23 00:14:16 +0200

    Modify domain provider naming

--- src/screens/pokemonList.js
+++ src/screens/pokemonList.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
 
-import {useDomain} from '../context/domainProvider'
+import {useDomain} from '../context/domain'
 
 export default function PokemonListScreen() {
   const domain = useDomain()
