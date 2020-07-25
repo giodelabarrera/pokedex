98f5df54d2833c402a5bd4a908503636d7b829d1
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-07-25 13:20:17 +0200

    Add intersection-observer polyfill

--- src/hooks/useIntersection/index.js
+++ src/hooks/useIntersection/index.js
@@ -1,29 +1,34 @@
 import {useEffect, useState} from 'react'
-// import 'intersection-observer'
 
 export default function useIntersection({target, root, rootMargin, threshold}) {
   const [isIntersecting, setIsIntersecting] = useState(false)
 
   useEffect(() => {
-    function handler(entries) {
-      setIsIntersecting(entries[0].isIntersecting)
-    }
+    Promise.resolve(() => {
+      return typeof IntersectionObserver !== 'undefined'
+        ? IntersectionObserver
+        : import('intersection-observer')
+    }).then(() => {
+      function handler(entries) {
+        setIsIntersecting(entries[0].isIntersecting)
+      }
 
-    const observer = new IntersectionObserver(handler, {
-      root: root && root.current,
-      rootMargin,
-      threshold
+      const observer = new IntersectionObserver(handler, {
+        root: root && root.current,
+        rootMargin,
+        threshold
+      })
+
+      const element = target && target.current
+      if (!element) return
+
+      observer.observe(element)
+
+      return () => {
+        setIsIntersecting(false)
+        observer.unobserve(element)
+      }
     })
-
-    const element = target && target.current
-    if (!element) return
-
-    observer.observe(element)
-
-    return () => {
-      setIsIntersecting(false)
-      observer.unobserve(element)
-    }
   }, [root, rootMargin, target, threshold])
 
   return isIntersecting
