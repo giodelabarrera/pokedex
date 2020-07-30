38515c9daa45c31b2d2f46772510941fe6249cbe
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-07-30 18:53:12 +0200

    Clean query to go other page

--- src/components/shared/header/searchForm/index.js
+++ src/components/shared/header/searchForm/index.js
@@ -1,31 +1,23 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 import SearchIcon from './searchIcon'
 import './index.scss'
 
-export default function SearchForm({initialValue = '', placeholder, onSubmit}) {
-  const [value, setValue] = useState(initialValue)
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    setValue(value)
-    onSubmit(value)
+export default function SearchForm({value = '', placeholder, onChange}) {
+  function handleChange(e) {
+    onChange(e.target.value)
   }
-
-  const handleChange = e => setValue(e.target.value)
 
   return (
     <div className="pk-SharedHeader-searchForm">
-      <form onSubmit={handleSubmit}>
-        <SearchIcon />
-        <input
-          className="pk-SharedHeader-searchForm-input"
-          type="text"
-          value={value}
-          placeholder={placeholder}
-          onChange={handleChange}
-        />
-      </form>
+      <SearchIcon />
+      <input
+        className="pk-SharedHeader-searchForm-input"
+        type="text"
+        value={value}
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
     </div>
   )
 }
