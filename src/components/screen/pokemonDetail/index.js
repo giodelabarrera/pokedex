d78e322389d16b047885ecc5f30a56bc428aaaa5
Giorgio de la Barrera <giodelabarrera@gmail.com>
2021-08-12 11:57:27 +0200

    Use useGetPokemonQuery provided by rtk-query

--- src/components/screen/pokemonDetail/index.js
+++ src/components/screen/pokemonDetail/index.js
@@ -1,9 +1,5 @@
-// import React, {useCallback} from 'react'
 import React from 'react'
 import {useParams} from 'react-router-dom'
-
-// import {useDomain} from 'context/domain'
-// import useAsync from 'hooks/useAsync'
 
 import PokemonDetail, {
   PokemonDetailContentLoader
@@ -14,34 +10,14 @@ import {useGetPokemonQuery} from 'services/pokemon'
 const baseClass = 'pk-ScreenPokemonDetail'
 
 export default function ScreenPokemonDetail() {
-  // const domain = useDomain()
-  // const {idOrSlug} = useParams()
-
-  // const fetchGetPokemonUseCase = useCallback(
-  //   () => domain.get('pokemon__get_pokemon_use_case').execute({idOrSlug}),
-  //   [domain, idOrSlug]
-  // )
-
-  // const {data, isLoading, error} = useAsync(fetchGetPokemonUseCase)
-
-  // return (
-  //   <div className={baseClass}>
-  //     {isLoading && <PokemonDetailContentLoader />}
-  //     {error && <ErrorFeedback error={error} />}
-  //     {data && <PokemonDetail pokemon={data} />}
-  //   </div>
-  // )
-
   const {idOrSlug} = useParams()
-
   const {data, isLoading, error} = useGetPokemonQuery(idOrSlug)
 
   return (
     <div className={baseClass}>
       {isLoading && <PokemonDetailContentLoader />}
       {error && <ErrorFeedback error={error} />}
-      {/*data && <PokemonDetail pokemon={data} />} */}
-      {data && JSON.stringify(data, null, 2)}
+      {data && <PokemonDetail pokemon={data} />}
     </div>
   )
 }
