11cb8961c96def128403511bbcfcd5360e21f4df
Giorgio de la Barrera <giodelabarrera@gmail.com>
2021-08-13 01:26:45 +0200

    Create useInfinitePokemonList hook

---
+++ src/components/pokemon/useInfinitePokemonList/index.js
@@ -0,0 +1,44 @@
+import {useState, useEffect} from 'react'
+
+import {useListPokemonQuery} from 'services/pokemon'
+
+export default function useInfinitePokemonList({query, limit, sort, offset}) {
+  const [historicalData, setHistoricalData] = useState()
+
+  const firstListPokemonState = useListPokemonQuery(
+    {query, limit, sort},
+    {skip: offset !== 0}
+  )
+
+  const restListPokemonState = useListPokemonQuery(
+    {query, limit, sort, offset},
+    {skip: offset === 0}
+  )
+
+  useEffect(() => {
+    if (firstListPokemonState.data) {
+      setHistoricalData(firstListPokemonState.data)
+    }
+  }, [firstListPokemonState.data])
+
+  useEffect(() => {
+    if (restListPokemonState.data) {
+      setHistoricalData(prevData => ({
+        ...restListPokemonState.data,
+        results: prevData.results.concat(restListPokemonState.data.results)
+      }))
+    }
+  }, [restListPokemonState.data])
+
+  const canLoadMore = historicalData
+    ? historicalData.total > historicalData.results.length
+    : false
+
+  return {
+    canLoadMore,
+    data: historicalData,
+    error: firstListPokemonState.error || restListPokemonState.error,
+    isLoading: firstListPokemonState.isLoading,
+    isLoadingMore: restListPokemonState.isLoading
+  }
+}
