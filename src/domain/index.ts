6dcf021b5128b37ccaa6206d8d8eef0a90508c1d
Giorgio de la Barrera <giodelabarrera@gmail.com>
2023-08-22 16:16:10 +0200

    create domain

---
+++ src/domain/index.ts
@@ -0,0 +1,12 @@
+import createDomain from "./createDomain";
+
+const useCases = {
+  'pokemon__get_pokemon_list_use_case': () =>
+    import("./pokemon/useCases/GetPokemonListUseCase/factory"),
+  'pokemon__get_pokemon_use_case': () =>
+    import("./pokemon/useCases/GetPokemonUseCase/factory"),
+};
+
+const domain = createDomain(useCases)
+
+export default domain
