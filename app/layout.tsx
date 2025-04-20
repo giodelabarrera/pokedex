ebb44451bd7d9b9d99d663cf7641b6b5f1dace7b
Giorgio de la Barrera <giodelabarrera@gmail.com>
2025-04-20 20:50:01 +0200

    Add header for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import NextLink from "next/link";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { forwardRef } from "react";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +15,102 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body data-theme="dark">
+        <div className="pk-App">
+          <Header />
+          <main>{children}</main>
+        </div>
       </body>
     </html>
+  );
+}
+
+function Header() {
+  const baseClass = "pk-SharedHeader";
+  return (
+    <header className={baseClass}>
+      <div className={`${baseClass}-content`}>
+        <NextLink href="/" className={`${baseClass}-logoContainer`}>
+          <Logo />
+        </NextLink>
+        <div className={`${baseClass}-offset`} />
+        <div className={`${baseClass}-searchContainer`}>
+          <form id="search-form" role="search">
+            <Search placeholder="Name or Number" defaultValue={""} />
+          </form>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+function Logo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="40"
+      height="40"
+      viewBox="0 0 1024 1024"
+      fill="currentColor"
+    >
+      <path
+        strokeWidth="1"
+        d="M 512.00,96.80
+           C 304.28,96.94 132.17,249.33 101.24,448.41
+             101.24,448.41 312.51,448.80 312.51,448.80
+             339.50,364.37 418.60,303.25 512.00,303.20
+             605.25,303.31 684.24,364.33 711.33,448.61
+             711.33,448.61 922.53,448.80 922.53,448.80
+             891.82,249.60 719.75,97.06 512.00,96.80
+             512.00,96.80 512.00,96.80 512.00,96.80 Z
+           M 512.00,376.80
+           C 436.89,376.80 376.00,437.69 376.00,512.80
+             376.00,587.91 436.89,648.80 512.00,648.80
+             512.00,648.80 512.00,648.80 512.00,648.80
+             587.11,648.80 648.00,587.91 648.00,512.80
+             648.00,512.80 648.00,512.80 648.00,512.80
+             648.00,437.69 587.11,376.80 512.00,376.80
+             512.00,376.80 512.00,376.80 512.00,376.80
+             512.00,376.80 512.00,376.80 512.00,376.80 Z
+           M 101.47,576.80
+           C 132.18,776.00 304.25,928.54 512.00,928.80
+             719.72,928.66 891.83,776.27 922.76,577.19
+             922.76,577.19 711.49,576.80 711.49,576.80
+             684.50,661.23 605.40,722.35 512.00,722.40
+             418.75,722.29 339.76,661.27 312.67,576.99
+             312.67,576.99 101.47,576.80 101.47,576.80
+             101.47,576.80 101.47,576.80 101.47,576.80 Z"
+      />
+    </svg>
+  );
+}
+
+const Search = forwardRef<
+  HTMLInputElement,
+  { defaultValue: string; placeholder: string }
+>(function Search({ defaultValue = "", placeholder }, ref) {
+  const baseClass = "pk-SharedHeader-search";
+  return (
+    <div className={baseClass}>
+      <SearchIcon />
+      <input
+        id="query"
+        ref={ref}
+        className={`${baseClass}-input`}
+        aria-label="Search pokemon"
+        type="text"
+        defaultValue={defaultValue}
+        placeholder={placeholder}
+        name="query"
+      />
+    </div>
+  );
+});
+
+function SearchIcon() {
+  return (
+    <svg height="20" width="20" viewBox="0 0 24 24" role="img">
+      <path d="M10 16c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6m13.12 2.88l-4.26-4.26A9.842 9.842 0 0 0 20 10c0-5.52-4.48-10-10-10S0 4.48 0 10s4.48 10 10 10c1.67 0 3.24-.41 4.62-1.14l4.26 4.26a3 3 0 0 0 4.24 0 3 3 0 0 0 0-4.24"></path>
+    </svg>
   );
 }
