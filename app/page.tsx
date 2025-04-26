abe64fc45a6acebc22ce5deda8f8d083d0f778b4
Giorgio de la Barrera <giodelabarrera@gmail.com>
2025-04-27 01:42:41 +0200

    Add first home results and detail result

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,100 @@
-export default function Home() {
-  return <h1>Hello Next.js!</h1>;
+import Link from "next/link";
+
+import { getPokemonList } from "@/lib/pokemon";
+
+export default async function Home() {
+  const pokemonList = await getPokemonList();
+
+  const baseClass = "pk-ScreenPokemonList";
+
+  return (
+    <div className={baseClass}>
+      {/* <div className={`${baseClass}-filterBar`}>
+        <FilterSort value={sort} onChange={handleFilterSortChange} />
+      </div> */}
+      <div className={`${baseClass}-content`}>
+        {pokemonList.total ? (
+          <>
+            <PokemonList pokemonList={pokemonList.results}>
+              {({ number, name, imageUrl, slug, types }) => (
+                <PokemonCard
+                  number={number}
+                  name={name}
+                  imageUrl={imageUrl}
+                  types={types}
+                  link={makePokemonDetailLink(slug)}
+                />
+              )}
+            </PokemonList>
+          </>
+        ) : (
+          <NoSearchResults />
+        )}
+      </div>
+    </div>
+  );
+}
+
+function makePokemonDetailLink(slug) {
+  return function (props) {
+    return <Link {...props} href={`/p/${slug}`} />;
+  };
+}
+
+function NoSearchResults() {
+  const baseClass = "pk-ScreenPokemonList-noSearchResults";
+  return (
+    <div className={baseClass}>
+      <h2 className={`${baseClass}-title`}>No Pokémon matched your search!</h2>
+      <p>Try searching for part of your number or name</p>
+    </div>
+  );
+}
+
+function PokemonList({ pokemonList, children }) {
+  const baseClass = "pk-PokemonList";
+  return (
+    <div className={baseClass}>
+      {pokemonList.map((pokemon) => (
+        <div className={`${baseClass}-item`} key={pokemon.id}>
+          {children(pokemon)}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function PokemonCard({ number, name, imageUrl, types, link: Link }) {
+  const baseClass = "pk-PokemonCard";
+
+  return (
+    <div className={baseClass}>
+      <div className={`${baseClass}-imageContainer`}>
+        <Link>
+          <img src={imageUrl} alt={name} />
+        </Link>
+      </div>
+      <div className={`${baseClass}-info`}>
+        <div className={`${baseClass}-titleContainer`}>
+          {name} <span className={`${baseClass}-number`}>#{number}</span>
+        </div>
+        <div className={`${baseClass}-typesContainer`}>
+          <div className={`${baseClass}-types`}>
+            {types.map((type) => (
+              <div className={`${baseClass}-typeItem`} key={type}>
+                <PokemonType type={type} size="small" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function PokemonType({ type, size = "medium" }) {
+  const baseClass = "pk-PokemonType";
+  return (
+    <div className={`${baseClass} ${baseClass}--${size} u-${type}`}>{type}</div>
+  );
 }
